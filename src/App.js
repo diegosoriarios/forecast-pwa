6397be148e705072b4e0ab53fa6581579cbedbfd
diego.rios <diego.rios@CTNAPPLE01YG8WN.local>
2018-12-10 21:54:44 -0200

    Fazendo meu proprio pwa

--- src/App.js
+++ src/App.js
@@ -3,73 +3,21 @@ import { NumericTextBox } from '@progress/kendo-react-inputs';
 import { Button } from "@progress/kendo-react-buttons";
 import './App.css';
 import '@progress/kendo-theme-default/dist/all.css';
+import Band from './Band';
+import Header from './Header';
 
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      habitId: 0,
-      habitName: '',
-      habitIteration: 0,
-      habits: []
     }
-  }
-
-  handleNameChange = (e) => {
-    this.setState({habitName: e.target.value});
-  }
-
-  handleInterationChange = (e) => {
-    this.setState({habitIteration: e.target.value});
-  }
-
-  handleHabit = (e) => {
-    this.setState({
-      habits: this.state.habits.concat([{
-        key: this.state.habitId,
-        name: this.state.habitName,
-        iteration: this.state.habitIteration
-      }]),
-      habitId: this.state.habitId + 1
-    });
   }
 
   render() {
     return (
-      <div className="App">
-        <header className="header">
-          <h1>Goals & Interations</h1>  
-        </header>
-        <div className="habits-list">
-          <ul>
-            {this.state.habits.map((habit, index) => [
-              <li key={index}>
-                <p>{habit.name}</p>
-                <div className="iterations-area">
-                  {[...Array(habit.iterations)].map((iteration, index) => {
-                    return <input key={index} type="radio" />
-                  })}
-                </div>
-              </li>
-            ])}
-          </ul>
-        </div>
-        <div className="add-habit">
-          <label>Goal: </label>
-          <input type="text" onChange={this.handleNameChange} />
-          <br />
-          <br />
-          <label>Interation </label>
-          <NumericTextBox 
-            format='0'
-            min={0}
-            max={22}
-            onChange={this.handleInterationChange} 
-          /><br /><br />
-          <Button primary={true} onClick={this.handleHabit}>
-            ASADASd
-          </Button>
-        </div>
+      <div className="app">
+        <Header />
+        <Band />
       </div>
     );
   }
---
+++ src/Band.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Band extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            name: '',
+            image: '',
+            link: '',
+            band: ''
+        }
+    }
+
+    callApi(band){
+        fetch(`http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=${band}&api_key=0c05709ba56254b354388302bb35460b&format=json`)
+        .then((result) => {
+          return result.json();
+        }).then((band) => {
+          let max = band.similarartists.artist.length;
+          let rand = Math.floor(Math.random() * max) + 1 ;
+          let artist = band.similarartists.artist[rand];
+          return artist;
+        }).then((artist) => {
+          let img = artist.image[5]['#text'];
+          this.setState({
+            name: artist.name,
+            image: img,
+            link: artist.url,
+          })
+        })
+      }
+
+    render() {
+        return(
+            <div className="band">
+                <input type="text" value={this.state.band} onChange={e => this.setState({band: e.target.value})} />
+                <button onClick={() => this.callApi(this.state.band)} >Busca</button>
+                <br /><br /><br />
+                
+                <h2>{this.state.name}</h2>
+                <a href={this.state.link}>{this.state.link}</a>
+                <img src={this.state.image} alt="image"/>                
+            </div>
+        )
+    }
+}
+
+export default Band;
